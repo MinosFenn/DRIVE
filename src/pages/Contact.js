5038ff61f76e8c6a310687988551b9baea17d7e4
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2020-11-16 17:56:16 +0100

    test form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -35,8 +35,39 @@ export default function Contact() {
           <h2>Nous contacter</h2>
 
 
-          <ContactForm />
-          </div>    
+          <div className="form">
+         <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  {/* You still need to add the hidden input with the form name to your JSX form */}
+  <input type="hidden" name="form-name" value="contact" />
+  <p>
+    <label>Votre Nom et Prénom <br></br><input required type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Votre Email: <br></br><input required type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Votre N° de Téléphone: <br></br><input required type="tel" name="tel" /></label>
+  </p>
+  <p>
+    <label for="requete">Votre demande concerne <br></br>
+    <select name="requete">
+      <option value="depot">Dépôt</option>
+      <option value="vente">Vente</option>
+      <option value="achat">Achat</option>
+      <option value="commande">Commande</option>
+      <option value="autre">Autre</option>
+
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <br></br><textarea required rows="15" name="message"></textarea></label>
+  </p>
+  <div data-netlify-recaptcha="true"></div>
+<p>
+    <button type="submit" className="submit-btn" >Envoyer</button>
+    </p>
+</form>
+        </div>          </div>    
           </div>
  
           </section>

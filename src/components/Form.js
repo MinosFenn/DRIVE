6d2cc634242e99227b920c5105329dd3976e5ab9
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2020-11-16 10:53:12 +0100

    form reworked

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Form() {
     return (
         <div className="form">
-          <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+          <form name="contact" method="POST"  data-netlify="true" data-netlify-recaptcha="true">
   <p>
     <label>Votre Nom et Prénom <br></br><input type="text" name="name" /></label>   
   </p>
